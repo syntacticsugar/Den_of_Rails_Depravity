fac41b6d4bb1fa342078f4992960d4e8591a2a12
RomyRomy :D <hello@romcreative.com>
2013-02-11 14:26:01 -0500

    updates gemfile

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c0122b44ce24611c6a3f7403069eca40636f88efb5a2275c47cf23b3cd711b0976e0820735ec7b1e42158564bc31a0f3712d74aaa176c48308939088f9375502'
